d12aa0eb14d58e1ac2cfb65ab76db84d242622e0
jsj1027 <javisjohnson1@gmail.com>
2018-03-19 14:19:04 -0400

    parser is a nightmare, remember + is not a digit but you need more ideas

--- src/MainCalc.py
+++ src/MainCalc.py
@@ -10,21 +10,43 @@ def main(argv):
     """
     w =  raw_input("Function Entry, Format(Num Operator Num): ")
     type(w)
-    return
+    w = list(w)
+    var = ""
+    operator = ""
+    total = []
+    counter = 0
+    total_count = len(w)
+    for temp in w:
+        while temp.isdigit():
+            var += str(temp)
+        else:
+            total.append(var)
+        while tem
+    print total
+    return None
+
+def operation_check(op):
+    if op : "+":
+        return True
 
 def addition(x,y):
     z = x + y
     print "The added result equals:", z
+    return None
 
 def subtraction(x,y):
     z = x - y
     print "The added result equals:", z
+    return None
 
 def multiplication(x,y):
     z = x * y
     print "The added result equals:", z
+    return None
 
 def division(x,y):
     z = float(x)/float(y)
     print "The added result equals:", z
+    return None
+
 main("h")
