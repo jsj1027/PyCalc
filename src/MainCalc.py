67c6fd6729b8a458819131dd0c1a574fab6cbdb5
jsj1027 <javisjohnson1@gmail.com>
2018-03-19 12:50:58 -0400

    Starting work on the parser

---
+++ src/MainCalc.py
@@ -0,0 +1,30 @@
+import math
+
+def main(argv):
+    """
+    x = raw_input("First number: ")
+    type(x)
+    y = raw_input("Second number: ")
+    type(y)
+    division(x,y)
+    """
+    w =  raw_input("Function Entry, Format(Num Operator Num): ")
+    type(w)
+    return
+
+def addition(x,y):
+    z = x + y
+    print "The added result equals:", z
+
+def subtraction(x,y):
+    z = x - y
+    print "The added result equals:", z
+
+def multiplication(x,y):
+    z = x * y
+    print "The added result equals:", z
+
+def division(x,y):
+    z = float(x)/float(y)
+    print "The added result equals:", z
+main("h")
