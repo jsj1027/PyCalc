ea9c66d99767398eb4d3ff325a28b71db8fb3017
jsj1027 <javisjohnson1@gmail.com>
2018-03-21 10:50:31 -0400

    upgradd to python 3 and changed a lot

--- src/MainCalc.py
+++ src/MainCalc.py
@@ -8,45 +8,45 @@ def main(argv):
     type(y)
     division(x,y)
     """
-    w =  raw_input("Function Entry, Format(Num Operator Num): ")
-    type(w)
+    w = input("Function Entry, Format(Num Operator Num): ")
     w = list(w)
     var = ""
     operator = ""
-    total = []
-    counter = 0
-    total_count = len(w)
+    total = {}
     for temp in w:
-        while temp.isdigit():
+        if temp.isdigit():
             var += str(temp)
         else:
             total.append(var)
-        while tem
-    print total
+    for temp in total:
+        print(temp)
     return None
 
 def operation_check(op):
-    if op : "+":
+    if op == "+":
         return True
+    else :
+        return False
+
 
 def addition(x,y):
     z = x + y
-    print "The added result equals:", z
+    print("The added result equals:", z)
     return None
 
 def subtraction(x,y):
     z = x - y
-    print "The added result equals:", z
+    print("The added result equals:", z)
     return None
 
 def multiplication(x,y):
     z = x * y
-    print "The added result equals:", z
+    print("The added result equals:", z)
     return None
 
 def division(x,y):
     z = float(x)/float(y)
-    print "The added result equals:", z
+    print("The added result equals:", z)
     return None
 
 main("h")
