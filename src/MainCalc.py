5ea9288b32997537c1421217eca523f636684377
jsj1027 <javisjohnson1@gmail.com>
2018-03-23 12:12:20 -0400

    Basic Calculator finished

--- src/MainCalc.py
+++ src/MainCalc.py
@@ -1,58 +1,70 @@
 import math
 
-def main(argv):
-    """
-    x = raw_input("First number: ")
-    type(x)
-    y = raw_input("Second number: ")
-    type(y)
-    division(x,y)
-    """
-    w = input("Function Entry, Format(Num Operator Num): ")
-    w = list(w)
-    var = ""
-    operator = ""
-    counter=len(w)
-    print(counter)
-    total = []
-    for i in range(0, counter) :
-        if w[i].isdigit() and i != counter-1 :
-            var += w[i]
-        elif not w[i].isdigit() and i != counter-1:
-            total.append(operation_check(w[i]))
-        elif w[i].isdigit() and w[i] == w[counter-1]:
-            var += w[i]
-            total.append(var)
-        else:
-            total.append(var)
-            var = ""
-    print (total)
+
+def main():
+    func_list = userinput()
+    total = inputparser(func_list)
+    if total[1] == "+":
+        addition(total[0], total[2])
+    elif total[1] == "-":
+        subtraction(total[0], total[2])
+    elif total[1] == "*":
+        multiplication(total[0], total[2])
+    elif total[1] == "/":
+        division(total[0], total[2])
     return None
+
+
+def userinput():
+    user_input = input("Function Entry, Format(Num Operator Num): ")
+    func_list = list(user_input)
+    return func_list
+
+def inputparser(func_list):
+    var = ""
+    counter = len(func_list)
+    total = []
+    for i in range(0, counter):
+        if func_list[i].isdigit() and i != counter - 1:
+            var += func_list[i]
+        elif not func_list[i].isdigit() and i != counter - 1:
+            total.append(var)
+            total.append(operation_check(func_list[i]))
+            var = ""
+        elif func_list[i].isdigit() and func_list[i] == func_list[counter - 1]:
+            var += func_list[i]
+            total.append(var)
+    return total
 
 def operation_check(op):
-    if op in ["+","-","*","/"] :
+    if op in ["+", "-", "*", "/"]:
         return op
-    else :
+    else:
         exit("Error")
 
-def addition(x,y):
-    z = x + y
-    print("The added result equals:", z)
+
+def addition(x, y):
+    z = float(x) + float(y)
+    print("The result is:", z)
     return None
 
-def subtraction(x,y):
-    z = x - y
-    print("The added result equals:", z)
+
+def subtraction(x, y):
+    z = float(x) - float(y)
+    print("The result is:", z)
     return None
 
-def multiplication(x,y):
-    z = x * y
-    print("The added result equals:", z)
+
+def multiplication(x, y):
+    z = float(x) * float(y)
+    print("The result is:", z)
     return None
 
-def division(x,y):
-    z = float(x)/float(y)
-    print("The added result equals:", z)
+
+def division(x, y):
+    z = float(x) / float(y)
+    print("The result is :", z)
     return None
 
-main("h")
+
+main()
